08f8eb3203b242c067a395fceb03924ab8bdc128
Hamza <hkkh1441@gmail.com>
2024-04-27 18:10:49 +0500

    Teachers structure done

--- src/ApiGateway/ApiGateway/obj/Debug/net7.0/ApiGateway.AssemblyInfo.cs
+++ src/ApiGateway/ApiGateway/obj/Debug/net7.0/ApiGateway.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("ApiGateway")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+407068314e4d58429fd35665b453dc58d2ef7e70")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+8c077e8a70cb37dbed232fe8015f92b6974cdd16")]
 [assembly: System.Reflection.AssemblyProductAttribute("ApiGateway")]
 [assembly: System.Reflection.AssemblyTitleAttribute("ApiGateway")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
---
+++ src/Services/Teachers/Teachers.Api/Controllers/TeachersController.cs
@@ -0,0 +1,9 @@
+using Microsoft.AspNetCore.Mvc;
+
+namespace Teachers.Api.Controllers;
+
+[ApiController]
+[Route("[controller]")]
+public class TeachersController : ControllerBase
+{
+}
